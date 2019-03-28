cae13f64812a8fc3ab7ffb636c9335cfc5b31968
Ronel Pantaleón <ronelpanag08@hotmail.com>
2019-03-28 00:03:35 -0400

    Agregar archivos de proyecto.

---
+++ Recursos Humanos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Recursos_Humanos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
